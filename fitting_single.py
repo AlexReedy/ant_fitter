33b3e4fa98c902881222d6d5c2d2fa71098dee7d
AlexReedy <alexander.h.reedy@gmail.com>
2021-12-01 15:43:26 -0800

    recreated fitting_single.py

---
+++ fitting_single.py
@@ -0,0 +1,35 @@
+from fittingLib import *
+fit = FittingLibrary(pause=.5, offset_prct=20, sigma_coefficient=5)
+
+
+ant_confirm_id = '1118060051368.dat'
+ant_test_id = '1118060050249.dat'
+
+fit.import_data(file=ant_confirm_id)
+fit.plot_raw()
+
+fit.plot_mag()
+fit.plot_flux()
+
+fit.polyfit_sigma_clipping()
+fit.plot_sigma_clip()
+
+fit.get_average()
+fit.plot_avg()
+
+fit.get_fit_parameters()
+fit.plot_fit_parameters()
+
+'''
+fit.plot_mag(save=True, show=False)
+fit.plot_flux(save=True, show=False)
+
+fit.sigma_clipping()
+fit.plot_sigma_clip(save=True, show=False)
+
+fit.get_average()
+fit.plot_sigma_clip_avg(save=True, show=False)
+
+fit.get_fit_parameters()
+fit.plot_fitting_parameters(save=True, show=False)
+'''
